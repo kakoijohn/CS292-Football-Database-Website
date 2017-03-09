12f6fc857f5faeba732d38f7fe9ea8b907edab8b
John Damits <kakoijohn@gmail.com>
2017-03-08 19:21:08 -0600

    Refactored some code

--- Java/Calculator.java
+++ Java/Calculator.java
@@ -1,8 +1,5 @@
 import java.util.HashMap;
 import java.util.List;
-
-import dbdemo.DataMapper;
-
 import java.util.Arrays;
 
 public class Calculator {
@@ -16,8 +13,8 @@ public class Calculator {
 		dMapper = new DataMapper();
 		mapList = dMapper.getMaps();
 		drive = mapList.get(0);
-		field = mapList.get(0);
-		punt = mapList.get(0);
+		field = mapList.get(1);
+		punt = mapList.get(2);
 	}
 	
 	public static void main(String[] args) {
@@ -26,14 +23,17 @@ public class Calculator {
 		System.out.println(play);
 	}
 	
+	/* returns number corresponding to the play that has the highest expected points value
+	 * given the distance to 1st down and starting yardline
+	 */
 	public int calculation(List<Integer> key) {
-	   	 if(key.size() != 2 && drive.isEmpty() && punt.isEmpty() && field.isEmpty()){
+	   	 if(key.size() != 2 || drive.isEmpty() || punt.isEmpty() || field.isEmpty()){
 	   		 return -1;
-	   	 }   			 
+	   	 }
 	   	 
 	   	 int[] expectedPoints = new int[3];
 	   	 expectedPoints[0]= 7*drive.get(key).get(0)/drive.get(key).get(1);
-	   	 expectedPoints[1]= 0*punt.get(key).get(0)/punt.get(key).get(1);//not sure about this so we kept it
+	   	 expectedPoints[1]= 0*punt.get(key).get(0)/punt.get(key).get(1); //not sure about this
 	   	 expectedPoints[2]= 3*field.get(key).get(0)/field.get(key).get(1);
 	   	 
 	   	 int indexOfHighest = 0;
@@ -45,50 +45,6 @@ public class Calculator {
 	   	 }   	 
 	   	 
 	   	 return indexOfHighest;
-	}
-
-	public void calc2(){
-		String[] output = new String[100];
-		output[0] = "Yardline,";
-		for(int x = 0; 15 > x;x++){
-		   	output[0] = output[0] + "4th & " + x + ": GFI,"
-		   				 		+ "4th & " + x + ": P,"
-		   				 		+ "4th & " + x + ": FG,";
-		}
-		
-		for(int yardline = 0; 100 > yardline;yardline++){
-			output[yardline] = yardline + ",";
-		   		 
-		   	for(int distance = 0; 15 > distance;distance++){   			 
-		   		List<Integer> temp = Arrays.asList(distance, yardline);
-		   		calculation(temp);
-		   			 
-		   			 
-		   	}
-		}
-	 
-		   	 
-		String hi = "Yard Line,";
-	  	for(int x = 0; 45 > x;x++){
-	  		String temp = "4th &" + x;
-		}   					 
-	   	 
-	}
-	    
-	/*
-	This method accepts the HashMaps for the Drive, Punt, and Field Goal data.
-	It is assumped that each of these Hashmaps will be Integer Lists with the
-	key being (distance, yardline) and val is (sum, n).
-	    
-	Some invalid inputs will return with -1.
-	    
-	It calculated the expectedPoints for each of the three plays, and returns an
-	index to the best play that will probably lead to the best score  
-	*/
-	public static int expectedPoints(int points, List<Integer> key, HashMap<List<Integer>, List<Integer>> play){
-
-	   	 return points * play.get(key).get(0)/play.get(key).get(1);
-
 	}
 	
 }
--- Java/DataMapper.java
+++ Java/DataMapper.java
@@ -4,28 +4,81 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
-// Requires MySQL-connector. 
-// To add, right-click project file in eclipse >> Build path >> Add External Archives.
-// Requires access to MySQL database, info below must match.
+/* Requires MySQL-connector. 
+ * To add, right-click project file in eclipse >> Build path >> Add External Archives.
+ * Requires access to MySQL database.
+ * Should be refactored to use get methods to access data instead of returning data structure.
+ */
 public class DataMapper {
-	static String url = "jdbc:mysql://localhost/d3football?";
-	static String username = "root";
-	static String password = "admin";
+	private String url;
+	private String username;
+	private String password;
+	private Connection connection;
+	private ResultSet results;
+	// Due to testing issues, this works if a play is a punt, field goal and drive at the same time
+	// TO DO: check if it works with the real data
+	private String query = "SELECT plays.distance, plays.location, " + 
+			"goforit.success, field_goals.success, punts.net\n" +
+			"FROM plays\n" + 
+			"INNER JOIN goforit\n" +
+			"ON (plays.id = goforit.play_id)\n" +
+			"INNER JOIN field_goals\n" +
+			"ON (plays.id = field_goals.play_id)\n" +
+			"INNER JOIN punts\n" +
+			"ON (plays.id = punts.play_id)\n" +
+			"WHERE plays.down = 4;";
 	
+	DataMapper() {
+		url = "jdbc:mysql://localhost/d3football?";
+		username = "root";
+		password = "admin";
+	}
+	
+	DataMapper(String url, String username, String password) {
+		this.url = url;
+		this.username = username;
+		this.password = password;
+	}
+	
+	
+	/* Return list of maps for results of each type of play
+	 * Key = [distance, yardline].
+     * Value = [number of successes OR total net punt distance, number of attempts].
+     */
 	public List<HashMap<List<Integer>, List<Integer>>> getMaps() {		
-		// JDBC objects.
-		Connection connection = null;
-        Statement statement = null;
-        ResultSet results = null;
+	    List<HashMap<List<Integer>, List<Integer>>> mapList = 
+        		new ArrayList<HashMap<List<Integer>, List<Integer>>>();
+		
+        try {
+			// Establish connection.
+		    connection = DriverManager.getConnection(url + "user=" + username + 
+		    									"&password="+password);
+		    
+		    // Query and get results.
+			Statement statement = connection.createStatement();
+		    results = statement.executeQuery(query);
+		   
+		    // Extract data from results row by row
+		    mapList = parseResults();
+		    
+        } catch (SQLException ex) {
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
         
-        // Maps for the results of each play option.
+		return mapList;
+	}
+	
+	private List<HashMap<List<Integer>, List<Integer>>> parseResults() {
+		// Maps for the results of each play option.
         // Key = [distance, yardline].
         // Value = [number of successes OR total net punt distance, number of attempts].
         HashMap<List<Integer>, List<Integer>> driveMap = new HashMap<>();
 	    HashMap<List<Integer>, List<Integer>> fieldMap = new HashMap<>();
 	    HashMap<List<Integer>, List<Integer>> puntMap = new HashMap<>();
 	    
-	    // List to hold previous 3 maps. Return value. [driveMap
+	    // List to hold previous 3 maps. Return value.
 	    List<HashMap<List<Integer>, List<Integer>>> mapList = 
         		new ArrayList<HashMap<List<Integer>, List<Integer>>>();
 	    
@@ -34,27 +87,8 @@ public class DataMapper {
 	    List<Integer> newValList = null;
 	    List<Integer> oldValList = null;
 		
-        try {
-			// Establish connection.
-		    connection = DriverManager.getConnection(url + "user=" + username + 
-		    									"&password="+password);
-		    
-		    // Create query and get results.
-			statement = connection.createStatement();
-		    String query = "SELECT plays.distance, plays.location, " + 
-		    				"goforit.success, field_goals.success, punts.net\n" +
-		    				"FROM plays\n" + 
-		    				"INNER JOIN goforit\n" +
-							"ON (plays.id = goforit.play_id)\n" +
-							"INNER JOIN field_goals\n" +
-							"ON (plays.id = field_goals.play_id)\n" +
-							"INNER JOIN punts\n" +
-							"ON (plays.id = punts.play_id)\n" +
-							"WHERE plays.down = 4;";
-		    results = statement.executeQuery(query);
-		   
-		    // Extract data from results row by row
-		    while(results.next()){
+	    try {
+			while(results.next()){
 		    	// Get column values
 		        Integer distance = results.getInt("distance");
 		        Integer location = results.getInt("location");
@@ -65,63 +99,64 @@ public class DataMapper {
 		        // Create map key to be used for all 3 maps.
 		        keyList = Arrays.asList(distance, location);
 		        
-		        // Add results to drive map.
-		        // If key not in map yet, create new entry.
-		        if (!driveMap.containsKey(keyList)) {
-		        	driveMap.put(keyList, Arrays.asList(driveSuccess, 1));
-		        }
-		        // If key already in map, replace the value with an updated list.
-		        else {
-		        	oldValList = driveMap.get(keyList);
-		        	newValList = Arrays.asList(oldValList.get(0)+driveSuccess,
-		        												oldValList.get(1)+1);
-		        	driveMap.put(keyList, newValList);
-		        }
-		        
-		        // Add results to field goal map.
-		        if (!fieldMap.containsKey(keyList)) {
-		        	fieldMap.put(keyList, Arrays.asList(fieldSuccess, 1));
-		        }
-		        else {
-		        	oldValList = fieldMap.get(keyList);
-		        	newValList = Arrays.asList(oldValList.get(0)+fieldSuccess,
-		        												oldValList.get(1)+1);
-		        	fieldMap.put(keyList, newValList);
+		        // Add results to drive map if play is a drive attemp.
+		        if (driveSuccess != null) {
+		        	// If key not in map yet, create new entry.
+			        if (!driveMap.containsKey(keyList)) {
+			        	driveMap.put(keyList, Arrays.asList(driveSuccess, 1));
+			        }
+			        // If key already in map, replace the value with an updated list.
+			        else {
+			        	oldValList = driveMap.get(keyList);
+			        	newValList = Arrays.asList(oldValList.get(0)+driveSuccess,
+			        												oldValList.get(1)+1);
+			        	driveMap.put(keyList, newValList);
+			        }
 		        }
 		        
-		        // Add results to punt map.
-		        if (!puntMap.containsKey(keyList)) {
-		        	puntMap.put(keyList, Arrays.asList(puntNet, 1));
-		        }
-		        else {
-		        	oldValList = puntMap.get(keyList);
-		        	newValList = Arrays.asList(oldValList.get(0)+puntNet,
-		        												oldValList.get(1)+1);
-		        	puntMap.put(keyList, newValList);
-		        }
+		        // Add results to field goal map if play is a field goal attempt.
+			    if (fieldSuccess != null) {
+		        	if (!fieldMap.containsKey(keyList)) {
+			        	fieldMap.put(keyList, Arrays.asList(fieldSuccess, 1));
+			        }
+			        else {
+			        	oldValList = fieldMap.get(keyList);
+			        	newValList = Arrays.asList(oldValList.get(0)+fieldSuccess,
+			        												oldValList.get(1)+1);
+			        	fieldMap.put(keyList, newValList);
+			        }
+				}
+			        
+		        // Add results to punt map if play is a punt.
+			    if (puntNet != null) {
+			        if (!puntMap.containsKey(keyList)) {
+			        	puntMap.put(keyList, Arrays.asList(puntNet, 1));
+			        }
+			        else {
+			        	oldValList = puntMap.get(keyList);
+			        	newValList = Arrays.asList(oldValList.get(0)+puntNet,
+			        												oldValList.get(1)+1);
+			        	puntMap.put(keyList, newValList);
+			        }
+			    }
 		    }
-		    
-        } catch (SQLException ex) {
-		    // Handle any errors.
+			
+	    } catch (SQLException ex) {
 		    System.out.println("SQLException: " + ex.getMessage());
 		    System.out.println("SQLState: " + ex.getSQLState());
 		    System.out.println("VendorError: " + ex.getErrorCode());
 		}
-        
-        // Add completed maps to list and return it.
-		mapList.add(driveMap);
+	    
+	    mapList.add(driveMap);
 		mapList.add(fieldMap);
 		mapList.add(puntMap);
 		return mapList;
 	}
 	
-	
 	public static void main(String[] args) {
 		DataMapper dMapper = new DataMapper();
 		List<HashMap<List<Integer>, List<Integer>>> mapList = dMapper.getMaps();
 		HashMap<List<Integer>, List<Integer>> driveMap = mapList.get(0);
-		//HashMap<List<Integer>, List<Integer>> fieldMap = mapList.get(0);
-		//HashMap<List<Integer>, List<Integer>> puntMap = mapList.get(0);
 		System.out.println("Drive Results");
 		for(List<Integer> key : driveMap.keySet()) {
 			System.out.println("Distance: " + key.get(0) + "  Yardline: " + key.get(1) +
